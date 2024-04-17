e08993b6ec98011ea7af63a8f34fc1e1066a6564
rathourpankaj1999@gmail.com <rathourpankaj1999@gmail.com>
2024-04-17 08:15:41 +0000

    Edited slot_book

--- client_code/slot_book/__init__.py
+++ client_code/slot_book/__init__.py
@@ -1,26 +1,26 @@
 from ._anvil_designer import slot_bookTemplate
 from anvil import *
 import anvil.server
-# Import the datetime module
-from datetime import datetime
+from datetime import datetime, timedelta
+
 class slot_book(slot_bookTemplate):
-  def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
-    self.init_components(**properties)
-    button_2_click()
+    def __init__(self, **properties):
+        self.init_components(**properties)
+        self.set_button_texts()
    
+    def set_button_texts(self):
+        current_date = datetime.now()
+        for i in range(1, 5):  # Assuming you have 4 buttons named button_1, button_2, ..., button_4
+            button_name = f'button_{i}'
+            button = getattr(self, button_name)
+            date_for_button = current_date + timedelta(days=i-1)
+            # Format the date as "Day" (e.g., "Tue") and day number (e.g., "17")
+            formatted_date = date_for_button.strftime("%a %d")
+            button.text = formatted_date
 
-# Define a function to set the text of the button to the current date and day name
+    def button_1_click(self, **event_args):
+        pass
 
-    # Any code you write here will run before the form opens.
-
-  def button_2_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    current_date = datetime.now()
-    # Format the date as "Day Name, Month Day, Year" (e.g., "Wednesday, April 15, 2024")
-    formatted_date = current_date.strftime("%A, %B %d, %Y")
-    
-    # Set the text of button_2 to the formatted date
-    self.button_2.text = formatted_date
-    
-    
+    def button_5_click(self, **event_args):
+      """This method is called when the button is clicked"""
+      pass

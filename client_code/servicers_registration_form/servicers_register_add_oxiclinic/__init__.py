51fb8fd516dea887869094c51e2902b3b0c8c8ab
mhpraveen1997@gmail.com <mhpraveen1997@gmail.com>
2024-04-17 05:02:48 +0000

    Created hospital.png, oxi_gym.png, oxi_wheel.png, oxi_wheel_2.png and 2 more; Edited servicers_registration_form.servicers_register_add_oxiclinic, settings and servicers_registration_form.services_register_add_service

--- client_code/servicers_registration_form/servicers_register_add_oxiclinic/__init__.py
+++ client_code/servicers_registration_form/servicers_register_add_oxiclinic/__init__.py
@@ -11,3 +11,11 @@ class servicers_register_add_oxiclinic(servicers_register_add_oxiclinicTemplate)
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def date_picker_1_change(self, **event_args):
+    """This method is called when the selected date changes"""
+    pass
+
+  def back_button_2_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('servicers_registration_form.services_register_add_service')
--- client_code/servicers_registration_form/services_register_add_service/__init__.py
+++ client_code/servicers_registration_form/services_register_add_service/__init__.py
@@ -11,3 +11,13 @@ class services_register_add_service(services_register_add_serviceTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def button_5_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('servicers_registration_form.servicers_registration_form_main')
+
+  def add_oxiclinic_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('servicers_registration_form.servicers_register_add_oxiclinic')
+
+  

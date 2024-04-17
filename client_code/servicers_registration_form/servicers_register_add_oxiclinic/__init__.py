6e02529a648deeeaf62432be8126b1e99bf72d24
mhpraveen1997@gmail.com <mhpraveen1997@gmail.com>
2024-04-17 13:25:06 +0000

    Edited servicers_registration_form.servicers_register_add_oxiclinic, settings and servicers_registration_form.servicers_registration_form_main

--- client_code/servicers_registration_form/servicers_register_add_oxiclinic/__init__.py
+++ client_code/servicers_registration_form/servicers_register_add_oxiclinic/__init__.py
@@ -10,16 +10,23 @@ class servicers_register_add_oxiclinic(servicers_register_add_oxiclinicTemplate)
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
-    # Any code you write here will run before the form opens.
-
-  def date_picker_1_change(self, **event_args):
-    """This method is called when the selected date changes"""
-    pass
-
+    # Any code you write here will run before the form opens
   def back_button_2_click(self, **event_args):
     """This method is called when the button is clicked"""
     open_form('servicers_registration_form.services_register_add_service')
 
   def next_button_1_click(self, **event_args):
     """This method is called when the button is clicked"""
-    open_form('servicers_registration_form.oxiclinic_documents')
+    hospital_name =self.hospital_name.text
+    establised_year =self.oxiclini_established_year.date
+    state = self.oxiclinic_state.text
+    district =self.oxiclinic_district.text
+    pincode = self.oxiclinic_pincode.text
+    address = self.oxiclinic_address.text
+    capsule = self.oxiclinic_capsules.text
+
+    if not hospital_name and not address and not capsule and not district and not establised_year and not pincode and not state:
+      pass
+    else:
+      oxiclinc_details =[hospital_name, establised_year, state, district, pincode, address, capsule]
+      open_form('servicers_registration_form.oxiclinic_documents')
--- client_code/servicers_registration_form/servicers_registration_form_main/__init__.py
+++ client_code/servicers_registration_form/servicers_registration_form_main/__init__.py
@@ -4,14 +4,12 @@ import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-import random
 import re
 
 class servicers_registration_form_main(servicers_registration_form_mainTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.auto_validate()
   
     # Any code you write here will run before the form opens.
   
@@ -22,66 +20,48 @@ class servicers_registration_form_main(servicers_registration_form_mainTemplate)
     password = self.servicers_password_text_box.text
     address = self.servicers_address_text_box.text
   
-    # random_code = self.generate_random_code()
-    # print(random_code)
-
-    # Validation logic
     email_regex = r'^[\w\.-]+@[\w\.-]+\.\w+$'
     is_valid_password, password_error_message = self.validate_password(password)
     if not name:
-      self.servicers_name_text_box.hint_text = 'please enter the name '
+      self.hint_text_2.text = 'please enter the name '
     elif not email or not re.match(email_regex, email):
-      self.servicers_email_text_box.placeholder = 'please enter the name '  
+      self.hint_text_2.text = 'Invalid email format. '  
     elif not is_valid_password:
-      self.servicers_password_text_box.placeholder = password_error_message 
+      print(password_error_message)
+      self.hint_text.text = password_error_message
     elif not phone or len(phone) != 10:
-      self.servicers_phone_text_box.placeholder = 'Please enter correct Phone number'
+      self.hint_text_2.text = 'Invalid Phone number.'
     elif not address:
-      self.servicers_address_text_box.placeholder ='Fill this field'
+      self.hint_text_2.text ='Fill the address field'
     else:
+      
       service_register_data = {'name': name, 'email': email,
                                   'phone': phone, 'address': address,
                                   'password': password, }
       """This method is called when the button is clicked"""
       open_form('servicers_registration_form.services_register_add_service')
-      
-        
-  def auto_validate(self, *args):
-    self.password_valid = bool(
-        self.servicers_password_text_box.text and self.validate_password(self.servicers_password_text_box.text)[
-            0])
+    
     # password validation
   def validate_password(self, password):
       # Check if the password is not empty
+    special_characters = r"[!@#$%^&*(),.?\":{}|<>]"
     if not password:
       return False, "Password cannot be empty"
-
       # Check if the password has at least 8 characters
-    if len(password) < 6:
+    elif len(password) < 6:
       return False, "Password must have at least 6 characters"
-
       # Check if the password contains both uppercase and lowercase letters
-    if not any(c.isupper() for c in password) or not any(c.islower() for c in password):
+    elif not any(c.isupper() for c in password) or not any(c.islower() for c in password):
       return False, "Password must contain uppercase, lowercase"
-
       # Check if the password contains at least one digit
-    if not any(c.isdigit() for c in password):
+    elif not any(c.isdigit() for c in password):
       return False, "Password must contain at least one digit"
-
       # Check if the password contains at least one special character
-    special_characters = r"[!@#$%^&*(),.?\":{}|<>]"
-    if not re.search(special_characters, password):
+    elif not re.search(special_characters, password):
       return False, "Password must contain a special character"
-
+    else :
       # All checks passed; the password is valid
-    return True, "Password is valid"
-
-  # def generate_random_code(self):
-  #   prefix = "SP"
-  #   random_numbers = ''.join(random.choices(string.digits, k=5))
-  #   code = prefix + random_numbers
-
-    # return code
+      return True, "Valid password"
 
   def show_password_check_box_change(self, **event_args):
     """This method is called when this checkbox is checked or unchecked"""

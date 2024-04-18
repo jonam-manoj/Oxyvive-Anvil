1d5a7c54008c785853487e1ef131cc6106fe7a5e
bejawadaprem@gmail.com <bejawadaprem@gmail.com>
2024-04-18 05:25:08 +0000

    Created dashboard.hospital_booking_form_1; Edited dashboard and settings

--- client_code/dashboard/__init__.py
+++ client_code/dashboard/__init__.py
@@ -8,3 +8,7 @@ class dashboard(dashboardTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def primary_color_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('dashboard.hospital_booking_form_1')

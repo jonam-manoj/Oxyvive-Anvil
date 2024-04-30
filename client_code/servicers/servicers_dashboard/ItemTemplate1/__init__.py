5866a74274fee9457972003ddb14f3c4ab4ac00d
mhpraveen1997@gmail.com <mhpraveen1997@gmail.com>
2024-04-30 06:56:35 +0000

    Edited servicers.servicers_dashboard.ItemTemplate1

--- client_code/servicers/servicers_dashboard/ItemTemplate1/__init__.py
+++ client_code/servicers/servicers_dashboard/ItemTemplate1/__init__.py
@@ -12,9 +12,7 @@ class ItemTemplate1(ItemTemplate1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    while True:
-      self.text_box_1.text=  datetime.now().time()
-      time.sleep(1)
+    
 
     # Any code you write here will run before the form opens.
 
@@ -22,6 +20,4 @@ class ItemTemplate1(ItemTemplate1Template):
     """This method is called when the button is clicked"""
     open_form('servicers.servicers_dashboard.customer_report')
 
-  def text_box_1_change(self, **event_args):
-    """This method is called when the text in this text box is edited"""
-    pass
+  

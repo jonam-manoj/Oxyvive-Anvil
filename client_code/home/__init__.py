8a4513929fa3e084beb2c5625fc2cae189a78997
sasivenkat143143@gmail.com <sasivenkat143143@gmail.com>
2024-05-30 11:55:23 +0000

    Edited home

--- client_code/home/__init__.py
+++ client_code/home/__init__.py
@@ -27,7 +27,83 @@ class home(homeTemplate):
   def button_5_copy_click(self, **event_args):
     """This method is called when the button is clicked"""
     pass
+
+  def link_8_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    self.label_64.visible=True
+    self.label_63.visible=False
+    self.label_65.visible=False
+    self.label_66.visible=False
+    self.label_67.visible=False
+    self.label_68.visible=False
+    self.label_69.visible=False
+  
+
+  def link_9_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    self.label_64.visible=False
+    self.label_63.visible=True
+    self.label_65.visible=False
+    self.label_66.visible=False
+    self.label_67.visible=False
+    self.label_68.visible=False
+    self.label_69.visible=False
+
+
+  def link_10_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    self.label_64.visible=False
+    self.label_63.visible=False
+    self.label_65.visible=True
+    self.label_66.visible=False
+    self.label_67.visible=False
+    self.label_68.visible=False
+    self.label_69.visible=False
+  
+
+  def link_11_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    self.label_64.visible=False
+    self.label_63.visible=False
+    self.label_65.visible=False
+    self.label_66.visible=True
+    self.label_67.visible=False
+    self.label_68.visible=False
+    self.label_69.visible=False
+  
+
+  def link_12_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    self.label_64.visible=False
+    self.label_63.visible=False
+    self.label_65.visible=False
+    self.label_66.visible=False
+    self.label_67.visible=True
+    self.label_68.visible=False
+    self.label_69.visible=False
    
+
+  def link_13_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    self.label_64.visible=False
+    self.label_63.visible=False
+    self.label_65.visible=False
+    self.label_66.visible=False
+    self.label_67.visible=False
+    self.label_68.visible=True
+    self.label_69.visible=False
     
+
+  def link_14_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    self.label_64.visible=False
+    self.label_63.visible=False
+    self.label_65.visible=False
+    self.label_66.visible=False
+    self.label_67.visible=False
+    self.label_68.visible=False
+    self.label_69.visible=True
+    
+
 
   

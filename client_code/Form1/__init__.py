3969b538ae6442b4e95cf09869287d1074461233
manoj4ev@gmail.com <manoj4ev@gmail.com>
2024-06-03 06:54:36 +0000

    Edited Form2 and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -4,6 +4,7 @@ import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+from ..Form2 import Form2
 
 
 class Form1(Form1Template):
@@ -12,3 +13,8 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    self.content_panel.clear()
+    self.content_panel.add_component(Form2())

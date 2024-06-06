bc46ee7240e662030aecb3ded9b53834cbe412e3
manoj4ev@gmail.com <manoj4ev@gmail.com>
2024-06-06 07:42:54 +0000

    Edited settings, slot_book and Form2

--- client_code/Form2/__init__.py
+++ client_code/Form2/__init__.py
@@ -24,9 +24,43 @@ class Form2(Form2Template):
             formatted_date = date_for_button.strftime("%a %d")
             button.text = formatted_date
 
+  def button_1_click(self, **event_args):
+      self.primary_color_1.visible=True
+      current_time = datetime.now().time()
+      times = ["09:00 AM", "11:00 AM", "1:00 PM", "3:00 PM", "5:00 PM", "7:00 PM"]
+      for i in range(5, 11):  # Assuming you have buttons named button_5 to button_11
+          button_name = f'button_{i}'
+          button = getattr(self, button_name)
+          # Assuming times list is predefined
+          button.text = times[i - 5] if i - 5 < len(times) else "No Time"  # Display time if available, otherwise display "No Time"
+          button_time = datetime.strptime(button.text, "%I:%M %p").time()
+          if current_time > button_time:
+              button.enabled = False  
+      for i in range(5, 11):
+            button_name = f'button_{i}'
+            button = getattr(self, button_name)
+            button.visible = True
+
+  def button_2_click(self, **event_args):
+      self.primary_color_1.visible=True
+      current_time = datetime.now().time()
+      times = ["09:00 AM", "11:00 AM", "1:00 PM", "3:00 PM", "5:00 PM", "7:00 PM"]
+      for i in range(5, 11):  # Assuming you have buttons named button_5 to button_11
+          button_name = f'button_{i}'
+          button = getattr(self, button_name)
+          # Assuming times list is predefined
+          button.text = times[i - 5] if i - 5 < len(times) else "No Time"  # Display time if available, otherwise display "No Time"
+          button_time = datetime.strptime(button.text, "%I:%M %p").time()
+          if current_time > button_time:
+              button.enabled = False  
+      for i in range(5, 11):
+            button_name = f'button_{i}'
+            button = getattr(self, button_name)
+            button.visible = True
+
   
 
-  def button_4_click(self, **event_args):
+  def button_3_click(self, **event_args):
       self.button_4.visible = False
       # self.button_4.visible=True
       current_time = datetime.now().time()
@@ -44,36 +78,24 @@ class Form2(Form2Template):
             button = getattr(self, button_name)
             button.visible = True
 
+  def button_4_click(self, **event_args):
+      self.primary_color_1.visible=True
+      current_time = datetime.now().time()
+      times = ["09:00 AM", "11:00 AM", "1:00 PM", "3:00 PM", "5:00 PM", "7:00 PM"]
+      for i in range(5, 11):  # Assuming you have buttons named button_5 to button_11
+          button_name = f'button_{i}'
+          button = getattr(self, button_name)
+          # Assuming times list is predefined
+          button.text = times[i - 5] if i - 5 < len(times) else "No Time"  # Display time if available, otherwise display "No Time"
+          button_time = datetime.strptime(button.text, "%I:%M %p").time()
+          if current_time > button_time:
+              button.enabled = False  
+      for i in range(5, 11):
+            button_name = f'button_{i}'
+            button = getattr(self, button_name)
+            button.visible = True
   def button_5_click(self, **event_args):
-      self.primary_color_1.visible=True
-      current_time = datetime.now().time()
-      times = ["09:00 AM", "11:00 AM", "1:00 PM", "3:00 PM", "5:00 PM", "7:00 PM"]
-      for i in range(5, 11):  # Assuming you have buttons named button_5 to button_11
-          button_name = f'button_{i}'
-          button = getattr(self, button_name)
-          # Assuming times list is predefined
-          button.text = times[i - 5] if i - 5 < len(times) else "No Time"  # Display time if available, otherwise display "No Time"
-          button_time = datetime.strptime(button.text, "%I:%M %p").time()
-          if current_time > button_time:
-              button.enabled = False  
-      for i in range(5, 11):
-            button_name = f'button_{i}'
-            button = getattr(self, button_name)
-            button.visible = True
-
-  def button_6_click(self, **event_args):
-      self.primary_color_1.visible=True
-      current_time = datetime.now().time()
-      times = ["09:00 AM", "11:00 AM", "1:00 PM", "3:00 PM", "5:00 PM", "7:00 PM"]
-      for i in range(5, 11):  # Assuming you have buttons named button_5 to button_11
-          button_name = f'button_{i}'
-          button = getattr(self, button_name)
-          # Assuming times list is predefined
-          button.text = times[i - 5] if i - 5 < len(times) else "No Time"  # Display time if available, otherwise display "No Time"
-          button_time = datetime.strptime(button.text, "%I:%M %p").time()
-          if current_time > button_time:
-              button.enabled = False  
-      for i in range(5, 11):
-            button_name = f'button_{i}'
-            button = getattr(self, button_name)
-            button.visible = True
+      """This method is called when the button is clicked"""
+      pass
+  
+ 

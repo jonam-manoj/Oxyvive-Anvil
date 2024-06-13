a63d45b6f14e0427e43285e01888b88bbcea0d37
manoj4ev@gmail.com <manoj4ev@gmail.com>
2024-06-13 10:44:57 +0000

    Edited Form3

--- client_code/Form3/__init__.py
+++ client_code/Form3/__init__.py
@@ -13,3 +13,17 @@ class Form3(Form3Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+    # Call the method to display wallet balance
+    self.display_wallet_balance()
+
+  def display_wallet_balance(self):
+          # Assuming there is only one record in the wallet_balance table
+          wallet_balance_record = app_tables.oxi_wallet.search()
+          if wallet_balance_record:
+              # Assuming wallet_balance column is a number
+              balance = wallet_balance_record[0]['wallet_balance']
+              # Set the text of label_5 to the wallet balance
+              self.label_5.text = str(balance)
+          else:
+              self.label_5.text = "No balance found"
+            
